7cc6cd115d18deb4834f4884f02d1778adaf9c1e
dragosh craciun <dragosh.craciun@gmail.com>
2018-01-22 20:32:01 +0100

    feat: Display first question
    Display first question on a single screen. Make radio buttons
    validate the user answer by displaying a toast.

--- app/src/main/java/com/galacticcoders/whowantstobeamartiancolonist/MainActivity.java
+++ app/src/main/java/com/galacticcoders/whowantstobeamartiancolonist/MainActivity.java
@@ -2,6 +2,10 @@ package com.galacticcoders.whowantstobeamartiancolonist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onRadioButtonClick1(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.hydrogen:
+                if (checked) {
+                    // Show validity of the answer as a toast
+                    Toast.makeText(this, "Right answer!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.helium:
+                if (checked) {
+                    // Show validity of the answer as a toast
+                    Toast.makeText(this, "Wrong answer!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.nitrogen:
+                if (checked) {
+                    // Show validity of the answer as a toast
+                    Toast.makeText(this, "Wrong answer!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.oxygen:
+                if (checked) {
+                    // Show validity of the answer as a toast
+                    Toast.makeText(this, "Wrong answer!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
