9e49f1d40d579edd73380c6d32fab01ddf4474e9
dragosh craciun <dragosh.craciun@gmail.com>
2018-01-24 19:39:58 +0100

    feat: Display third question with checboxes
    Implement the third question and displayed it with checkboxes.
    The score is validated and added to the results screen. Second
    question screen is now leading to the third question. Third question
    screen is now leading to the results page.v

--- app/src/main/java/com/galacticcoders/whowantstobeamartiancolonist/QuestionThree.java
+++ app/src/main/java/com/galacticcoders/whowantstobeamartiancolonist/QuestionThree.java
@@ -4,45 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RadioButton;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
-public class QuestionTwo extends AppCompatActivity {
+public class QuestionThree extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.question_two);
+        setContentView(R.layout.question_three);
     }
 
-    public void onRadioButtonClick(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
+    public void onCheckboxClick(View view) {
+        // Is the box now checked?
+        boolean checked = ((CheckBox) view).isChecked();
 
-        // Check which radio button was clicked
+        // Check which checkbox was clicked
         switch (view.getId()) {
-            case R.id.mars:
+            case R.id.argon:
                 if (checked) {
                     // Show validity of the answer as a toast
                     Toast.makeText(this, "Right answer!", Toast.LENGTH_SHORT).show();
                     MainActivity.correctAnswers++;
                 }
                 break;
-            case R.id.venus:
+            case R.id.carbon_dioxide:
+                if (checked) {
+                    // Show validity of the answer as a toast
+                    Toast.makeText(this, "Right answer!", Toast.LENGTH_SHORT).show();
+                    MainActivity.correctAnswers++;
+                }
+                break;
+            case R.id.methane:
                 if (checked) {
                     // Show validity of the answer as a toast
                     Toast.makeText(this, "Wrong answer!", Toast.LENGTH_SHORT).show();
                     MainActivity.wrongAnswers++;
                 }
                 break;
-            case R.id.mercury:
-                if (checked) {
-                    // Show validity of the answer as a toast
-                    Toast.makeText(this, "Wrong answer!", Toast.LENGTH_SHORT).show();
-                    MainActivity.wrongAnswers++;
-                }
-                break;
-            case R.id.jupiter:
+            case R.id.ammonia:
                 if (checked) {
                     // Show validity of the answer as a toast
                     Toast.makeText(this, "Wrong answer!", Toast.LENGTH_SHORT).show();
@@ -53,9 +53,9 @@ public class QuestionTwo extends AppCompatActivity {
     }
 
     // Intent to move to the next question
-    public void nextQuestion(View view) {
-        Intent nextQuestion = new Intent(this, QuestionThree.class);
-        startActivity(nextQuestion);
+    public void results(View view) {
+        Intent results = new Intent(this, ResultActivity.class);
+        startActivity(results);
     }
 
     /**
