62c15e7ec994ba8da933c933dd7841fb02388cd5
dragosh craciun <dragosh.craciun@gmail.com>
2018-01-27 15:06:48 +0100

    feat: Make app work in full-screen mode
    The app now works in full-screen mode with no action bar.# Please enter the commit message for your changes. Lines starting

--- app/src/main/java/com/galacticcoders/whowantstobeamartiancolonist/QuestionFive.java
+++ app/src/main/java/com/galacticcoders/whowantstobeamartiancolonist/QuestionFive.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -13,6 +15,9 @@ public class QuestionFive extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.question_five);
     }
 
--- app/src/main/java/com/galacticcoders/whowantstobeamartiancolonist/QuestionSix.java
+++ app/src/main/java/com/galacticcoders/whowantstobeamartiancolonist/QuestionSix.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -12,6 +14,9 @@ public class QuestionSix extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.question_six);
     }
 
